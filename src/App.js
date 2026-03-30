0181ba680b8a42b887d91aac734abd05d4ce48e2
kylemiller629 <kyleamiller629@gmail.com>
2026-03-30 14:03:42 -0500

    Initial react app with buildspec

--- src/App.js
+++ src/App.js
@@ -4,20 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Kyle Miller</h1>
+      <p>Cloud for Developers Project4</p>
     </div>
   );
 }
